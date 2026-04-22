81aa9cc5d42a1cdb24c85f814b4f5173ca445fa2
wtaype <wilder.taype16@gmail.com>
2026-04-22 10:00:02 -0500

    Actualizacion Principal v12.10.10

--- smiles/wii.js
+++ smiles/wii.js
@@ -6,16 +6,16 @@ export let lanzamiento = 2026;
 export let by = '@wilder.taype';
 export let linkme = 'https://wtaype.github.io/';
 export let ipdev = import.meta.env.VITE_DEV;
-export let version = 'v11';
+export let version = 'v12';
 
 /** ACTUALIZACIÓN PRINCIPAL ONE DEV [MAIN] (1)
-git add . ; git commit -m "Actualizacion Principal v11.10.10" ; git push origin main
+git add . ; git commit -m "Actualizacion Principal v12.10.10" ; git push origin main
 
 //  Actualizar versiones de seguridad [TAG NUEVO] (2)
-git tag v11 -m "Version v11" ; git push origin v11
+git tag v12 -m "Version v12" ; git push origin v12
 
 // Actualizar versiones de seguridad [TAG REMPLAZO] (3)
-git tag -d v11 ; git tag v11 -m "Version v11 actualizada" ; git push origin v11 --force
+git tag -d v12 ; git tag v12 -m "Version v12 actualizada" ; git push origin v12 --force
 
 // Actualizar versiones de seguridad [ELIMINAR CARPETA - ARCHIVO ONLINE] (4)
 git rm --cached skills-lock.json ; git commit -m "Archivo Eliminado" ; git push origin main
